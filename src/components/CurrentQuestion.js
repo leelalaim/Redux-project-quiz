400d52f5da7627e6bf988743910c0fe7c7a8a304
Laima Duhovnaja <laima@laimas-mbp.mynet>
2021-04-12 16:07:11 +0200

    next, radio buttons done

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { quiz } from '../reducers/quiz';
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
 
+  const answer = useSelector((state) =>
+    state.quiz.answers.find((a) => a.questionId === question.id)
+  );
+
+  const dispatch = useDispatch();
+  
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
@@ -11,6 +18,27 @@ export const CurrentQuestion = () => {
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      {question.options.map((option, index) => {
+        console.log(option.indexOf('Justin Gatlin'))
+        return (
+          <>
+            <label htmlFor={option}>{option}</label>
+            <input 
+              key={index} 
+              type="radio" 
+              id={index} 
+              name="contact"
+              disabled={answer !== undefined}
+              checked={answer !== undefined && answer.answerIndex === index}
+              onChange={() => dispatch(quiz.actions.submitAnswer({questionId: question.id, answerIndex: index}))}></input>
+          </>
+        )
+      })}
+      <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
+        Next
+      </button>
     </div>
   )
 }
+
+
