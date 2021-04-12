e20d6c55e09aad3737ea7ebd1c1ba69dc750b3bf
mmolliss <malin.vannesjo@gmail.com>
2021-04-12 22:07:50 +0200

    Fixed the summary path and the corresponding map

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { quiz } from '../reducers/quiz';
-
-const Summary = () => {
-
-  // const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
-  // if (!question) {
-  //    return <h1>Oh no! I could not find the current question!</h1>
-  //  }
-
-  const answer = useSelector((state) => state.quiz.initialState{state.quiz.answers})
-  console.log(answer)
-    return (
-      <div>
-          <h1>hello</h1>
-      </div>
- )
-}
+import { Summary } from './Summary';
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
-  const answer = useSelector((state) =>
-    state.quiz.answers.find((a) => a.questionId === question.id)
-  );
+  const answer = useSelector((state) => state.quiz.answers.find(
+    (a) => a.questionId === question.id
+  ));
 
   const dispatch = useDispatch();
-  
+
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
@@ -36,29 +19,27 @@ export const CurrentQuestion = () => {
     <div>
       <h1>Question: {question.questionText}</h1>
       {question.options.map((option, index) => {
-        console.log(option.indexOf('Justin Gatlin'))
         return (
           <>
             <label htmlFor={option}>{option}</label>
-            <input 
-              key={index} 
-              type="radio" 
-              id={index} 
+            <input
+              key={index}
+              type="radio"
+              id={index}
               name="contact"
               disabled={answer !== undefined}
               checked={answer !== undefined && answer.answerIndex === index}
-              onChange={() => dispatch(quiz.actions.submitAnswer({questionId: question.id, answerIndex: index}))}></input>
+              onChange={() => dispatch(quiz.actions.submitAnswer({
+                questionId: question.id, answerIndex: index
+              }))} />
           </>
         )
       })}
-      <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
+      <button type="submit" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
         Next
       </button>
       <Summary />
     </div>
   )
 }
-
-
-
 
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { quiz } from '../reducers/quiz'
-
 
 export const Summary = () => {
-
-     const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-
-     if (!question) {
-        return <h1>Oh no! I could not find the current question!</h1>
-      }
-
-    return (
-        <div>
-            <h1>{question}</h1>
-        </div>
-    )
+  const answers = useSelector((state) => state.quiz.answers)
+  console.log(answers)
+  return (
+    <>
+      {answers.map((answer) => {
+        return (
+          <div>
+            <h1>{answer.answer}</h1>
+          </div>
+        )
+      })}
+    </>
+  )
 }
-
-// const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
